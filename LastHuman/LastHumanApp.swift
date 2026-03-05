acc0016623149daeb8f487280dee4ca95a1fbe61
anuar_universe <serik.anuar.bolatuly@gmail.com>
2026-03-05 11:56:20 +0500

    Initial Commit

---
+++ LastHuman/LastHumanApp.swift
@@ -0,0 +1,17 @@
+//
+//  LastHumanApp.swift
+//  LastHuman
+//
+//  Created by Ануар Серик on 05.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct LastHumanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
